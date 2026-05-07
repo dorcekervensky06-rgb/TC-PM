158cf9ac989d1205256e889082379b41ecd869c4
Kervensky Dorcé <dorcekervensky06@gmail.com>
2026-05-06 22:39:34 -0400

    🔥 Add Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration for ISERSS-TC-PM
+const firebaseConfig = {
+  apiKey: "AIzaSyCTMN-6CPj_Z2xX3S3hbHVEhwQlNZuVmcI",
+  authDomain: "iserss-tc-pm.firebaseapp.com",
+  projectId: "iserss-tc-pm",
+  storageBucket: "iserss-tc-pm.firebasestorage.app",
+  messagingSenderId: "1045546199733",
+  appId: "1:1045546199733:web:462f6c7827a4fdc2dc2cfc",
+  measurementId: "G-29NKNSFV7L"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
